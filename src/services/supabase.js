24426c64fbfd9499a78db49f3817b0be2930ea12
idknevo <nevoexists@gmail.com>
2025-03-20 20:25:15 +0200

    removed env XDDDDD

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = "https://veyxykogcheakugfyagu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZleXh5a29nY2hlYWt1Z2Z5YWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwODUzNjgsImV4cCI6MjA1NTY2MTM2OH0.oREDibpI6gAg35-cM63riAtZLQrVJDqC3O9t7209VWI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
