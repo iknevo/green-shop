ecec654d1cb0cd8b719b25abd9f69f605385575c
idknevo <nevoexists@gmail.com>
2025-04-01 20:05:40 +0200

    style: header item after styles

--- src/ui/components/header/Header.jsx
+++ src/ui/components/header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderNav from "./HeaderNav";
 
 export default function Header() {
   return (
-    <header className="border-primary/50/50 border-primary/50 sticky top-0 z-50 flex h-30 items-center justify-between border-b-[0.3px] bg-white/50 py-7 backdrop-blur-3xl">
+    <header className="border-primary/50/50 border-primary/50 sticky top-0 z-50 flex h-30 items-center justify-between border-b-[0.3px] bg-white/50 backdrop-blur-3xl">
       <Link to="/">
         <Logo />
       </Link>
